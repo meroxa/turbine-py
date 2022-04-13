62b5f13f75ce5f593b6ea414c1040dc1723f10a6
janelletavares <janelle@meroxa.io>
2022-04-13 10:16:03 -0700

    force local when checking functions

--- src/turbine/runner/app_runner.py
+++ src/turbine/runner/app_runner.py
@@ -10,10 +10,15 @@ def run_app(path_to_data_app: str, runtime: str, image_name: str, *args, **kwarg
 
     from main import App
 
-    client = turbine.Turbine(runtime, path_to_data_app, image_name)
-    asyncio.run(App.run(client))
-
     if kwargs["command"] == "functions":
+        client = turbine.Turbine("local", path_to_data_app, image_name)
+        asyncio.run(App.run(client))
         asyncio.run(client.list_functions())
     elif kwargs["command"] == "hasFunctions":
+        client = turbine.Turbine("local", path_to_data_app, image_name)
+        asyncio.run(App.run(client))
         asyncio.run(client.has_functions())
+    else:
+        client = turbine.Turbine(runtime, path_to_data_app, image_name)
+        asyncio.run(App.run(client))
+        
