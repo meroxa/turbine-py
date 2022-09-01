1bbe2b072f75f21d29b53104d35ac5259df3328c
Eric Cheatham <eric@meroxa.io>
2022-09-01 16:29:32 -0500

    cleanup

--- src/turbine/runtime/intermediate.py
+++ src/turbine/runtime/intermediate.py
@@ -30,11 +30,11 @@ class IntermediateResource:
             config = {}
 
         if not collection:
-            raise Exception(f"A collection name is required for all resources")
+            raise Exception("A collection name is required for all resources")
 
         if self.has_source:
             raise Exception(
-                f"Only one call to Records() is allowed per Meroxa Data Application"
+                "Only one call to Records() is allowed per Meroxa Data Application"
             )
 
         self._persist("source", collection, config)
@@ -47,7 +47,7 @@ class IntermediateResource:
             config = {}
 
         if not collection:
-            raise Exception(f"A collection name is required for all resources")
+            raise Exception("A collection name is required for all resources")
 
         self._persist("destination", collection, config)
 
