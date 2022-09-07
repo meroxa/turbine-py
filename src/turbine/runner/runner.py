09c9aadf32a6a99cdd34f85404b3efc0033b0bca
janelletavares <janelle@meroxa.io>
2022-09-07 12:48:38 -0700

    consistency

--- src/turbine/runner/runner.py
+++ src/turbine/runner/runner.py
@@ -86,7 +86,7 @@ class Runner(BaseRunner):
 
         try:
             await self.data_app.run(deployment_spec)
-            print(f"turbine: {deployment_spec.serialize()}\n")
+            print(f"turbine-response: {deployment_spec.serialize()}\n")
             return
         except Exception as e:
             pprint.pprint(f"{e}")
