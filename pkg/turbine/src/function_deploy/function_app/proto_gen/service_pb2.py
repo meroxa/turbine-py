8b2c2bdf21743fdc339d502bf41d8bf87c466ef0
anna-cross <annakh@meroxa.io>
2023-05-18 11:58:01 -0400

    Reformat proto for lint

--- pkg/turbine/src/function_deploy/function_app/proto_gen/service_pb2.py
+++ pkg/turbine/src/function_deploy/function_app/proto_gen/service_pb2.py
@@ -17,7 +17,6 @@ DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, "service_pb2", globals())
 if _descriptor._USE_C_DESCRIPTORS == False:
-
     DESCRIPTOR._options = None
     DESCRIPTOR._serialized_options = b"Z\037github.com/meroxa/funtime/proto"
     _PROCESSRECORDREQUEST._serialized_start = 36
